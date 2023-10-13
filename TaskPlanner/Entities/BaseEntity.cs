d3e9e7c650c70bab5567317c040079e33c880829
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-10-13 15:32:10 -0300

    Criação das entidades

---
+++ TaskPlanner/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskPlanner.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ TaskPlanner/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskPlanner.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual List<Task>? Tasks { get; set; }
+
+        public User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
