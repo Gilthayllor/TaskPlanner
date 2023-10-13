1b0f4fbf6972c2316d721dd49d75bdf1f0bc5e3b
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-10-13 17:18:22 -0300

    Criação do profile de mapeamento de TaskItem.

---
+++ TaskPlanner/AutoMapper/TaskItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TaskPlanner.Entities;
+using TaskPlanner.ViewModels;
+
+namespace TaskPlanner.AutoMapper
+{
+    public class TaskItemProfile : Profile
+    {
+        public TaskItemProfile()
+        {
+            CreateMap<TaskItem, TaskItemViewModel>();
+        }
+    }
+}
