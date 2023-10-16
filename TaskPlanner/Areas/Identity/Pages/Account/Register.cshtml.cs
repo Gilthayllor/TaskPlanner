1204d6fa13a29aabc671e8c571ba448bf667f0b7
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-10-15 21:01:06 -0300

    Adição de páginas de registro e layout

---
+++ TaskPlanner/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TaskPlanner.Services.Interfaces;
+using TaskPlanner.ViewModels;
+
+namespace TaskPlanner.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly IUserService _userService;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+
+        [BindProperty]
+        public UserRegisterViewModel UserRegisterForm { get; set; } = new UserRegisterViewModel();
+
+        public string ErrorMessage { get; set; } = string.Empty;
+
+        public bool Success { get; set; }
+
+        public RegisterModel(IUserService userService, IHttpContextAccessor httpContextAccessor)
+        {
+            _userService = userService;
+            _contextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (_contextAccessor?.HttpContext?.User?.Identity?.IsAuthenticated ?? false)
+            {
+                return Redirect("/");
+            }
+
+            return Page();
+        }
+    }
+}
